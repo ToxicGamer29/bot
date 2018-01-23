949e9199fee923debd90b7f117497d7122944708
ToxicGamer29 <35739587+ToxicGamer29@users.noreply.github.com>
2018-01-23 11:29:03 -0600

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,1 +1,17 @@
+import discord
+from discord.ext.command import bot
+from discord.ext import commands
+import asyncio
+import time
+
+Client = discord.Client()
+client = command.Bot(command_prefix = "!")
+
+@client.event
+async def on_ready():
+	print("Bot is online!")
+	
+	
+	
+client.run("NDAxODQ1MTc1OTIxNDEwMDU4.DUkCIQ.GBLLlchlMWhCzfbPd9T-nmaJDWA")
 
